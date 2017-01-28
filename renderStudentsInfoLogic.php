b0a08d0706fe0e671760568a810297d5861a3aef
KosKam <palmag83@gmail.com>
2017-01-28 20:00:38 +0200

    edit

--- renderStudentsInfoLogic.php
+++ renderStudentsInfoLogic.php
@@ -5,6 +5,19 @@ $age = $_GET["ages"];
 $submited = $_GET["submited"];
 
 if (isset($name, $age)) {
-    
+    echo "<table border = 1>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>    
+</table>";
 }
 
