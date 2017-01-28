159c0cbc09ea58250fd1e0f7eec7f047508f1d72
KosKam <palmag83@gmail.com>
2017-01-28 19:32:49 +0200

    Render Students HTML

---
+++ renderStudentsInfoHTML.php
@@ -0,0 +1,26 @@
+<form method="get" action="renderStudentsInfoLogic.php">
+    <div>
+        <select name="separator">
+        <option value=",">,</option>
+        <option value="|">|</option>
+        <option value="&">&</option>
+        </select>
+    </div>
+    <div>
+        Names: 
+        <input type="text" name="names">        
+    </div>
+    <div>
+        Ages: 
+        <input type="text" name="ages">        
+    </div>
+    <div>     
+        <input type="submit" name="submited" value="Filter!">        
+    </div>   
+    
+</form>
+
+
+
+
+
