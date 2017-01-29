9ed640446a53900bd16994b43f5ceb7e4451f3fe
KosKam <palmag83@gmail.com>
2017-01-29 13:57:25 +0200

    Update renderStudentsInfoLogic.php

--- renderStudentsInfoLogic.php
+++ renderStudentsInfoLogic.php
@@ -1,11 +1,32 @@
 <?php
-
-$name = $_GET["names"];
-$age = $_GET["ages"];
+session_start();
+$name = $_GET["name"];
+$age = $_GET["age"];
 $submited = $_GET["submited"];
 
-if (isset($name, $age)) {
-    echo "<table border = 1>
+$nameStudent = array();
+$ageStudent = array();
+$i = 0;
+
+if (isset($submited, $name, $age)) {
+    
+    $nameStudent[$i] = $name;
+    $ageStudent[$i] = $age;
+    $i++;       
+}
+
+$_SESSION["nameStudent"] = $nameStudent;
+$_SESSION["ageStudent"] = $ageStudent;
+$_SESSION["i"] = 0;
+$_SESSION["i"]++;
+
+?>
+<?php    var_dump($_SESSION["i"]); ?>
+
+<?php if (isset($names, $ages)): ?> 
+
+    
+    <table border = 1>
     <thead>
         <tr>
             <th>Name</th>
@@ -13,11 +34,15 @@ if (isset($name, $age)) {
         </tr>
     </thead>
     <tbody>
+        <?php for($ii = 0; $ii < count($nameStudent); $ii++): ?>
         <tr>
-            <td></td>
-            <td></td>
+            <td><?= $nameStudent[$ii] ?></td>
+            <td><?= $ageStudent[$ii] ?></td>
         </tr>
+        <?php endfor; ?>
     </tbody>    
-</table>";
-}
+</table>;
 
+<?php endif; ?>
+
+ <?php include 'renderStudentsInfoHTML.php'; ?>
